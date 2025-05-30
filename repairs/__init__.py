0db02bd568815da56dbe339f1c3dd45fdf80df7f
MRiabov <maksymriabov2004@gmail.com>
2025-05-30 10:18:04 +0100

    Removing the core package from repairs to focus on benchmark (for this package)

--- repairs/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Repairs environment for robotic car repair tasks."""
-
-# This file is intentionally left empty to allow for package imports.
-# All configuration and implementation is in the respective module files.
